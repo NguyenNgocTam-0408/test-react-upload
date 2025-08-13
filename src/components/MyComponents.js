f57f7d21427e3f8aa81917589430ced94a483b15
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-08-13 10:12:50 +0700

    video DomEvent

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -5,10 +5,21 @@ class MyComponent extends React.Component {
     address: "hoid dan IT",
     age: 26
   };
+  handleclick(event)
+  {
+    console.log(">>>click me button");
+    console.log("My name is ", this.state.name);
+  }
+  handleOnMouseOver(event) {
+  console.log("di chuyet vao toi")
+}
   render() {
     return (
       <div>
+         
          My name is {this.state.name}, I am {this.state.age} years old, I live in {this.state.address}
+        <button onMouseOver={this.handleOnMouseOver}>Hover me</button>
+        <button onClick={this.handleclick}>click me</button>
       </div>
     );
   }
