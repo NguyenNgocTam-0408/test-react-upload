f5ed33acbc76258c181b14eee24f56c12f9cdd7e
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-09-06 01:03:02 +0700

    down bostrap react-dom

--- src/App.js
+++ src/App.js
@@ -1,38 +1,14 @@
-import logo from './logo.svg';
 import './App.scss'; 
-import { useDispatch, useSelector } from 'react-redux';
-import MyComponent from './components/MyComponents';
-import React from 'react';
 
-class App extends React.Component
+
+const App = () =>
 {
-  render(){
-     return (
-      <div className='app-container'> 
-        <MyComponent></MyComponent>
-        Hello word voi eric &amp; Hoi dan IT
+      return (
+        <div className="app-container">
+        hello world
       </div>
-     );
+      );
   }
-}
 
-// class App extends React.Component
-// {
-//   render(){
-//      return (
-//       <div className="App">
-//        <header className="App-header">
-//          <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <div>Count = {count}</div>
-//         <button onClick={() => dispatch(increaseCounter())}>Increase</button>
-//         <button onClick={() => dispatch(decreaseCounter())}>Decrease</button>
-//       </header>
-//     </div>
-//      );
-//   }
-// }
 
 export default App;
