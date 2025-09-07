4239e6dd8be38d742d565229ec260a4a7d2acec1
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-09-07 20:25:40 +0700

    tạo trang home index

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -8,7 +8,8 @@ const Header = () =>    {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Học React</Navbar.Brand>
+        <NavLink to="/" className="navbar-brand">Học React</NavLink>
+
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
---
+++ src/components/Home/Home.js
@@ -0,0 +1,8 @@
+const Home = () =>{
+    return(
+        <div className="homepage-container">
+            <h2>Welcome to React Router</h2>
+        </div>
+    )
+}
+export  default Home;
