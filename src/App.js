de0d44e70dfc30eba1f18c4463261464977ea6a4
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-09-07 16:27:12 +0700

    dien huong bang router

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.scss'; 
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 
 
 const App = () =>
@@ -7,7 +8,17 @@ const App = () =>
       return (
         <div className="app-container">
         <Header />
+        <div>
+        hello world 
       </div>
+      <button>
+        <Link to="/users">Go to user page</Link>
+      </button>
+      <button>
+        <Link to="/admins">Go to admin page</Link>
+      </button>
+    </div>
+      
       );
   }
 
