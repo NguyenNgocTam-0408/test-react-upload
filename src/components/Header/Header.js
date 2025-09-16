39239be4f5c768744d37a70270bda00e394939cb
Nguyen Ngoc Tam <ngoctamsts1@gmail.com>
2025-09-16 15:25:39 +0700

    sidebar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,14 +24,14 @@ const Header = () =>    {
           <Nav>
             <button className="btn-login">Login</button>
             <button className="btn-signup">Sign up</button>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
+            {/* <NavDropdown title="Settings" id="basic-nav-dropdown">
                         <NavDropdown.Item href="#action/3.1">Log in</NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.2">
                             Log out
                         </NavDropdown.Item>
                         <NavDropdown.Item href="#action/3.3">Profile</NavDropdown.Item>
                         
-                        </NavDropdown>
+                        </NavDropdown> */}
           </Nav>
 
         </Navbar.Collapse>
